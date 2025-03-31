91f86d08696b5dac57eeb0b5bf658d5e4fc6fb84
TimPeIT <timpella95@web.de>
2025-03-31 12:37:49 +0200

    traning basics python

---
+++ python-basics/math_operator.py
@@ -0,0 +1,19 @@
+
+# a = 3
+# b = 2
+
+# print(a + b)
+
+erste_zahl = int(input("Gib eine erste Zahl ein: "))
+zweite_zahl = int(input("Gib eine zweite Zahl ein: "))
+
+# ergebnis = erste_zahl + zweite_zahl 
+# print(ergebnis)
+
+def addition(a, b):
+    sum = a + b
+    print(sum)
+    return sum
+
+addition(erste_zahl, zweite_zahl)
+addition(2,3)
